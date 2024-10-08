234a514d7af7dfed039fb4693260888217c3dac5
frwny <y.milyaev@mail.com>
2024-10-08 14:45:37 +0100

    restructured directories

---
+++ colours.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#only works on bash >4
+
+declare -A colours
+
+colours["bg1"] =      "2E3440"
+colours["bg2"] =      "3B4252"
+colours["bg3"] =      "434C5E"
+colours["bg4"] =      "4C566A"
+
+colours["fg1"] =      "D8DEE9"
+colours["fg2"] =      "E5E9F0"
+colours["fg3"] =      "ECEFF4"
+
+colours["mint"] =     "8FBCBB"
+colours["cyan"] =     "88C0D0"
+colours["blue"] =     "81A1C1"
+colours["indigo"] =   "5E81AC"
+
+colours["red"] =      "BF616A"
+colours["orange"] =   "D08770"
+colours["yellow"] =   "EBCB8B"
+colours["green"] =    "A3BE8C"
+colours["magenta"] =  "B48EAD"
