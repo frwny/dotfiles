f43d32daa6515d05accef9b06802d49283ad9b69
Yegor Milyaev <y.milyaev@pm.me>
2025-08-05 15:04:21 +0100

    moved dashboard.nvim and lazy loaded plugins

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,26 +1,48 @@
 return {
--- vanity plugins
-  'dstein64/vim-startuptime',
+  -- vanity plugins
+    'dstein64/vim-startuptime',
   {
     'goolord/alpha-nvim',
+    lazy = false,
     config = function()
       require'alpha'.setup(require'alpha.themes.dashboard'.config)
     end
   },
 
--- git plugins
-  'tpope/vim-fugitive',
-  'airblade/vim-gitgutter',
+  -- git plugins
+  {
+    'tpope/vim-fugitive',
+    event = 'VeryLazy',
+  },
+  {
+    'airblade/vim-gitgutter',
+    event = 'VeryLazy',
+  },
 
--- lsp, syntax and linting plugins
+  -- lsp, syntax and linting plugins
   'https://github.com/mason-org/mason.nvim',
   -- 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   'j-hui/fidget.nvim',
 
--- utility plugins
-  'tpope/vim-commentary',
-  'lukas-reineke/indent-blankline.nvim',
-  'kylechui/nvim-surround',
-  'windwp/nvim-autopairs',
-  'stevearc/quicker.nvim',
+  -- utility plugins
+  {
+    'tpope/vim-commentary',
+    event = 'VeryLazy',
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'VeryLazy',
+  },
+  {
+    'kylechui/nvim-surround',
+    event = 'VeryLazy',
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "VeryLazy",
+  },
+  {
+    'stevearc/quicker.nvim',
+    event = "VeryLazy",
+  },
 }
