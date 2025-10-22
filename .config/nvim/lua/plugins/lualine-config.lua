57676f10c8f0ade45222a012eefce67da3293d6f
Yegor Milyaev <y.milyaev@pm.me>
2025-10-22 14:16:23 +0100

    updated lualine path

--- .config/nvim/lua/plugins/lualine-config.lua
+++ .config/nvim/lua/plugins/lualine-config.lua
@@ -27,7 +27,7 @@ return {
       lualine_b = {'branch', 'diff', 'diagnostics'},
       lualine_c = {{
         'filename',
-        path = 4
+        path = 1
       }},
       lualine_x = {'filetype'},
       lualine_y = {'progress'},
@@ -38,7 +38,7 @@ return {
       lualine_b = {},
       lualine_c = {{
         'filename',
-        path = 4
+        path = 1
       }},
       lualine_x = {'location'},
       lualine_y = {},
